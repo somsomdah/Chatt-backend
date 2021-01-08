87cb57a75a6f6a0ff85cac78d671e5bad2cf00e7
Ubuntu <ubuntu@ip-172-31-46-38.ap-northeast-2.compute.internal>
2021-01-09 01:19:16 +0900

    Update wsgi.py

--- server/wsgi.py
+++ server/wsgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.prod')
 
 application = get_wsgi_application()
+application = WhiteNoise(application,root='/home/ubuntu/srv/Chatt-project-backend/staticfiles')
+#application = DjangoWhiteNoise(application)
+
+
+
+
