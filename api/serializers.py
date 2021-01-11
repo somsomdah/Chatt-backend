f25b7087cdc61dbc5ba89f3c648d7601ea02127c
somsomdah <somdah98@gmail.com>
2021-01-11 20:52:45 +0900

    Update serializers.py, views.py

--- api/serializers.py
+++ api/serializers.py
@@ -9,7 +9,7 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ['last_login', 'date_joined', 'groups', 'user_permissions']
+        exclude = ['last_login', 'date_joined', 'groups', 'user_permissions','password']
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
